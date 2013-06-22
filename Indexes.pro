8980df7f0f6953ac9af06ef15fd2c1688bd0fb76
brallan <apcomptec@gmail.com>
2013-06-22 04:30:14 -0600

    Ejemplo B+

--- Indexes.pro
+++ Indexes.pro
@@ -27,9 +27,13 @@ HEADERS += \
     util/Converter.h \
     dataStructure/INode.h \
     dataStructure/DLLNode.h \
-    dataStructure/DLL.h
+    dataStructure/DLL.h \
+    example/bplustree/BPlusTree.h
 
 OTHER_FILES += \
     doc/Indexes_UML.xmi \
     doc/Indexes_UML.pdf
 
+LIBS += -lboost_system-mt -lboost_filesystem-mt
+
+unix: CONFIG += link_pkgconfig
--- main.cpp
+++ main.cpp
@@ -1,25 +1,31 @@
 #include <iostream>
 #include "HashTable.h"
+#include "example/bplustree/BPlusTree.h"
 
 using namespace std;
 
 int main()
 {
+    BPlusTree<int, int, 3, 2> b;
+    b.insert(8, 9);
+    b.insert(3, 4);
+    std::cout << b.find(8);
+    std::cout << b.find(2);
     // TABLA HASH
-    cout << "Hello Hash Table!" << endl;
-    HashTable *h = new HashTable();
-    cout << h->hashFuntion("D") << endl;
-    h->insertData("Ana");
-    h->insertData("Daniel");
-    h->insertData("Dario");
-    h->insertData("Dalia");
-    h->insertData("Dash");
-    h->printHashTable();
-    //h->searchData("D");
-    h->deleteData("D", 3);
-    h->printHashTable();
-    h->insertData("Donaldo");
-    h->printHashTable();
+//    cout << "Hello Hash Table!" << endl;
+//    HashTable *h = new HashTable();
+//    cout << h->hashFuntion("D") << endl;
+//    h->insertData("Ana");
+//    h->insertData("Daniel");
+//    h->insertData("Dario");
+//    h->insertData("Dalia");
+//    h->insertData("Dash");
+//    h->printHashTable();
+//    //h->searchData("D");
+//    h->deleteData("D", 3);
+//    h->printHashTable();
+//    h->insertData("Donaldo");
+//    h->printHashTable();
     return 0;
 }
 
